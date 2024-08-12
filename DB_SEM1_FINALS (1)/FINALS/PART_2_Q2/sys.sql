780bc3f2043e92166a733c1a81897ca4279c926e
Meetpatel6118 <120136269+Meetpatel6118@users.noreply.github.com>
2024-08-12 11:58:11 -0400

    Add files via upload

---
+++ DB_SEM1_FINALS (1)/FINALS/PART_2_Q2/sys.sql
@@ -0,0 +1,3 @@
+connect sys/sys as sysdba
+CREATE USER Meet_FINAL_Q1 IDENTIFIED BY 6118 ;
+GRANT connect, resource TO Meet_FINAL_Q1 ;    
---
+++ DB_SEM1_FINALS (1)/FINALS/PART_2_Q3/sys.sql
@@ -0,0 +1,3 @@
+connect sys/sys as sysdba
+CREATE USER Meet_Patel_FINAL_Q2 IDENTIFIED BY 6118 ;
+GRANT connect, resource TO Meet_Patel_FINAL_Q2 ;    
